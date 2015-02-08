9c1ee7fb28995018b6b63401ccdd6981cb966b25
javaboyka <zzyzhenyang@126.com>
2015-02-08 22:26:42 +0800

    zzy - should reduce one chance when guess wrong

--- java/maven/src/main/java/GameProgress.java
+++ java/maven/src/main/java/GameProgress.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
 
 /**
@@ -5,13 +7,29 @@ import java.io.PrintStream;
  */
 public class GameProgress {
     private PrintStream out;
+    private final BufferedReader reader;
+    private final GuessNumber guessNumber;
+    private final GetRandomNumber getRandomNumber;
 
-    public GameProgress(PrintStream out) {
+    public GameProgress(PrintStream out, BufferedReader reader, GuessNumber guessNumber, GetRandomNumber getRandomNumber) {
         this.out = out;
+        this.reader = reader;
+        this.guessNumber = guessNumber;
+        this.getRandomNumber = getRandomNumber;
     }
 
-    public void start() {
+    public void start() throws IOException {
         out.println("welcome!");
         out.println("please input your number(6):");
+
+        String answer = getRandomNumber.getRandomNumber();
+        String input = reader.readLine();
+        String tips = guessNumber.getTips(input, answer);
+
+        out.println(tips);
+        if(!"4A0B".equals(tips)){
+            out.println("please input your number(5):");
+        }
+
     }
 }
--- java/maven/src/test/java/GameProgressTest.java
+++ java/maven/src/test/java/GameProgressTest.java
@@ -5,23 +5,34 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InOrder;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
 
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 public class GameProgressTest {
 
     private PrintStream out;
     private GameProgress game;
+    private BufferedReader reader;
+    private GetRandomNumber getRandomNumber;
 
     @Before
-    public void setUp(){
+    public void setUp() throws IOException {
         out = mock(PrintStream.class);
 
-        game = new GameProgress(out);
+        reader = mock(BufferedReader.class);
+        GuessNumber guessNumber = new GuessNumber();
+        getRandomNumber = mock(GetRandomNumber.class);
+        given(reader.readLine()).willReturn("1234");
+        given(getRandomNumber.getRandomNumber()).willReturn("4321");
+
+        game = new GameProgress(out, reader, guessNumber, getRandomNumber);
     }
     @Test
-    public void should_print_welcome_when_game_start(){
+    public void should_print_welcome_when_game_start() throws IOException {
 
         verify(out, never()).println("welcome!");
         game.start();
@@ -29,12 +40,24 @@ public class GameProgressTest {
     }
 
     @Test
-    public void should_print_input_after_game_start(){
+    public void should_print_input_after_game_start() throws IOException {
 
         game.start();
         InOrder inOrder = inOrder(out);
         inOrder.verify(out).println("welcome!");
         inOrder.verify(out).println("please input your number(6):");
 
+    }
+
+    @Test
+    public void should_reduce_one_chance_when_guess_wrong() throws IOException {
+
+        game.start();
+
+        InOrder inOrder = inOrder(out);
+        inOrder.verify(out).println("welcome!");
+        inOrder.verify(out).println("please input your number(6):");
+        inOrder.verify(out).println("0A4B");
+        inOrder.verify(out).println("please input your number(5):");
     }
 }
